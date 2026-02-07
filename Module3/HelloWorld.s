0dfb809963e272187cbf3cb282c93a4a02323f0e
Kim <sp26a42d47@rpi-buster-armhf>
2026-02-07 02:53:00 +0000

    Added Hello World

--- Module3/HelloWorld.s
+++ Module3/HelloWorld.s
@@ -1,25 +1,23 @@
-
 .text
+.global main
+main:
+	SUB sp, sp, #4
+	STR lr, [sp]
 
+	LDR r0, =HelloWorld
+	BL printf
 
-
+	LDR lr, [sp]
+	ADD sp, sp, #4
+	MOV pc, lr
 .data
+	helloworld: .asciz "Hello World\n"
+
+#End main
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-:wq
 
