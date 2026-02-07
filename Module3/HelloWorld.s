1865838281af79d72940493652037cdb044ca056
Kim <sp26a42d47@rpi-buster-armhf>
2026-02-07 03:04:36 +0000

    complete Module3

--- Module3/HelloWorld.s
+++ Module3/HelloWorld.s
@@ -4,14 +4,14 @@ main:
 	SUB sp, sp, #4
 	STR lr, [sp]
 
-	LDR r0, =HelloWorld
+	LDR r0, =helloWorld
 	BL printf
 
 	LDR lr, [sp]
 	ADD sp, sp, #4
 	MOV pc, lr
 .data
-	helloworld: .asciz "Hello World\n"
+	helloWorld: .asciz "Hello World\n"
 
 #End main
 
