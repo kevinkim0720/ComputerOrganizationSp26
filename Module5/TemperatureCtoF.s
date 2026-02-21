41271088d159ebec4f3393d29671f3dcc935ad2e
Kim <sp26a42d47@rpi-buster-armhf>
2026-02-21 22:43:02 +0000

    Finished celsius to fahrenheit

--- Module5/TemperatureCtoF.s
+++ Module5/TemperatureCtoF.s
@@ -1,7 +1,38 @@
+# Program Name: TemperatureCtoF.s
+# Author: Kevin Kim
+# Date: 2/21/2026
+# Purpose: Convert Celsius to Fahrenheit
 .text
 .global main
+main:
+	SUB sp, sp, #4
+	STR lr, [sp]
 
+	LDR r0, =prompt
+	BL printf
 
+	LDR r0, =formatString
+	LDR r1, =celsius
+	BL scanf
+
+	LDR r0, =celsius
+	LDR r0, [r0]
+	MOV r1, #9
+	MUL r0, r0, r1
+	MOV r1, #5
+	BL __aeabi_idiv
+	MOV r1, #32
+	ADD r0, r0, r1
+
+	MOV r1, r0
+	LDR r0, =output
+	Bl printf	
+
+	LDR lr, [sp]
+	ADD sp, sp, #4
+	MOV pc, lr
 .data
-
-
+	prompt: .asciz "Enter degrees in Celsius: "
+	celsius: .word 0
+	formatString: .asciz "%d"
+	output: .asciz "That is %d Degrees Fahrenheit\n"
