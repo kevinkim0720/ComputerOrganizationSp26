1b070ae6e1e0f41ba0b9538ad7db2eb3edfbe3be
Kim <sp26a42d47@rpi-buster-armhf>
2026-03-19 14:49:48 +0000

    Grade program

--- Module9/libFunctions.s
+++ Module9/libFunctions.s
@@ -26,7 +26,45 @@ findMAXOf3:
 
 .text
 Grade:
+	SUB sp, sp, #8
+	STR lr, [sp]
+	STR r4, [sp, #4]
 
+	# Store for safety
+	MOV r4, r0
+
+	MOV r0, #0
+	CMP r4, #0
+	ADDGE r0, r0, #1
+	MOV r1, #0
+	CMP r4, #100
+	ADDLE r1, r1, #1
+	OR r0, r0, r1
+
+	CMP r1, #1
+	BNE ErrorMsg
+		# Grade is valid
+		CMP r4, #90
+		BGE elsif_1:
+			LDR r0, =grade_A
+			BL printf
+			
+	ErrorMsg:
+		"Print if grade is invalid"
+		LDR r0, =error
+		BL printf
+	EndError
+
+	LDR lr, [sp]
+	LDR r4, [sp, #4]
+	ADD sp, sp, #8
+	MOV pc, lr
 .data
+	error: .asciz ""
+	grade_A: .asciz "\nGrade is A"
+	grade_B: .asciz "\nGrade is B"
+	grade_C: .asciz "\nGrade is C"
+	grade_D: .asciz "\nGrade is D"
+	grade_F: .asciz "\nGrade is F"
 
 #END Grade
--- Module9/main.s
+++ Module9/main.s
@@ -1,10 +1,34 @@
 .text
 .global main
 main:
+	SUB sp, sp, #4
+	STR lr, [sp, #0]
 
+
+	#Grade program
+	LDR r0, =promptGrade
+	BL printf
+
+	LDR r0, =inputInt
+	LDR r1, =grade
+	BL scanf
+
+	LDR r0, =grade
+	LDR r0, [r0]
+	BL Grade
+
+	LDR lr, [sp, #0]
+	ADD sp, sp, #4
+	MOV pc, lr
 
 .data
 promptLOG: .asciz "Input something and I'll tell you if it's alphabetic using logical operations: "
 promptNOLOG: .asciz "Input something and I'll tell you if it's alphabetic not using logical operations: "
 promptMax: .asciz "Input 3 values: "
-promptGrade: .asciz "What was your score: "
+promptGrade: .asciz "Enter a score between 0-100: "
+
+inputChar: .asciz "%s"
+inputInt: .asciz "%d"
+
+
+grade: .word 0
