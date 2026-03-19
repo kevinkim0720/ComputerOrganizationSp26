20fd249f812f4a061029dd937671b9961156e4b0
Kim <sp26a42d47@rpi-buster-armhf>
2026-03-19 13:51:43 +0000

    Started assignment

---
+++ Module9/libFunctions.s
@@ -0,0 +1,32 @@
+.global ChkAlphaLOG
+.global ChkAlphaNOLOG
+.global findMAXOf3
+.global Grade
+
+.text
+ChkAlphaLOG:
+
+.data
+
+#END ChkAlphaLOG
+
+.text
+ChkAlphaNOLOG:
+
+.data
+
+#END ChkAlphaNOLOG
+
+.text
+findMAXOf3:
+
+.data
+
+#END findMAXOf3
+
+.text
+Grade:
+
+.data
+
+#END Grade
---
+++ Module9/main.s
@@ -0,0 +1,10 @@
+.text
+.global main
+main:
+
+
+.data
+promptLOG: .asciz "Input something and I'll tell you if it's alphabetic using logical operations: "
+promptNOLOG: .asciz "Input something and I'll tell you if it's alphabetic not using logical operations: "
+promptMax: .asciz "Input 3 values: "
+promptGrade: .asciz "What was your score: "
