59db33f07d7242ab74e92d3827db7d21dcf05d04
Kim <sp26a42d47@rpi-buster-armhf>
2026-03-20 17:05:07 +0000

    Added student to Grade program

--- Module9/libFunctions.s
+++ Module9/libFunctions.s
@@ -30,7 +30,7 @@ Grade:
 	STR lr, [sp]
 	STR r4, [sp, #4]
 
-	# Store for safety
+	# Store grade score safely
 	MOV r4, r0
 
 	MOV r0, #0
@@ -40,6 +40,8 @@ Grade:
 	CMP r4, #100
 	ADDLE r1, r1, #1
 	AND r0, r0, r1
+
+	MOV r1, #0
 
 	CMP r0, #1
 	BNE ErrorMsg
@@ -60,33 +62,28 @@ Grade:
 
 	gradeA:
 		LDR r0, =grade_A
-		BL printf
 		B EndError
 
 	gradeB:
 		LDR r0, =grade_B
-		BL printf
 		B EndError
 
 	gradeC:
 		LDR r0, =grade_C
-		BL printf
 		B EndError
 
 	gradeD:
 		LDR r0, =grade_D
-		BL printf
 		B EndError
 
 	gradeF:
 		LDR r0, =grade_F
-		BL printf
 		B EndError
 			
 	ErrorMsg:
 		# Print if grade is invalid
 		LDR r0, =error
-		BL printf
+		MOV r1, #1
 
 	EndError:
 		LDR lr, [sp]
@@ -95,10 +92,10 @@ Grade:
 		MOV pc, lr
 .data
 	error: .asciz "Invalid score\n"
-	grade_A: .asciz "Grade is A\n"
-	grade_B: .asciz "Grade is B\n"
-	grade_C: .asciz "Grade is C\n"
-	grade_D: .asciz "Grade is D\n"
-	grade_F: .asciz "Grade is F\n"
+	grade_A: .asciz "A"
+	grade_B: .asciz "B"
+	grade_C: .asciz "C"
+	grade_D: .asciz "D"
+	grade_F: .asciz "F"
 
 #END Grade
--- Module9/main.s
+++ Module9/main.s
@@ -6,6 +6,14 @@ main:
 
 
 	#Grade program
+
+	LDR r0, =promptStudent
+	BL printf
+
+	LDR r0, =inputStr
+	LDR r1, =student
+	BL scanf
+
 	LDR r0, =promptGrade
 	BL printf
 
@@ -17,6 +25,21 @@ main:
 	LDR r0, [r0]
 	BL Grade
 
+	CMP r1, #1
+	BEQ print_invalid
+	
+	MOV r2, r0
+	LDR r0, =printGrade
+	LDR r1, =student
+	BL printf
+	B done
+
+	print_invalid:
+	LDR r0, = invalid_msg
+	BL printf
+
+	done:
+
 	LDR lr, [sp, #0]
 	ADD sp, sp, #4
 	MOV pc, lr
@@ -25,10 +48,16 @@ main:
 promptLOG: .asciz "Input something and I'll tell you if it's alphabetic using logical operations: "
 promptNOLOG: .asciz "Input something and I'll tell you if it's alphabetic not using logical operations: "
 promptMax: .asciz "Input 3 values: "
+promptStudent: .asciz "Input student name: "
 promptGrade: .asciz "Enter a score between 0-100: "
 
-inputChar: .asciz "%s"
+
+printGrade: .asciz "%s got grade letter %s\n"
+
+inputStr: .asciz "%s"
 inputInt: .asciz "%d"
 
-
+student: .space 40
 grade: .word 0
+
+invalid_msg: .asciz "Invalid score\n"
