6c35c97fa5dc9b15ee0740017266f5297fb01c2c
Kim <sp26a42d47@rpi-buster-armhf>
2026-04-02 23:05:31 +0000

    fixed initial

---
+++ Module10/primeTester.s
@@ -0,0 +1,11 @@
+.text
+.global main
+main:
+	# Program dictionary:
+	# r4 - counter for prime tester loop
+	# r5 - 
+
+.data
+promptCont: .asciz "\nEnter -1 to end the program, any other key to continue: "
+promptNum: .asciz "\nEnter a number to test: "
+
